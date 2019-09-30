6fb470ca8de21310e9a1756d0eca93e86c57446e
Duong Van Son <duongvansonit@gmail.com>
2019-09-30 21:45:47 +0700

    30/09/2019

---
+++ Dot NET/CodeLearn/LostMessage/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostMessage
+{
+    class Program
+    {
+        static string LostMessage(string[] message)
+        {
+            List<String> R = new List<string>();
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i].Substring(0, 1) == "R")
+                {
+                    var arr = message[i].Split(':');
+                    R.Add(arr[1]);
+                    message[i] = "XXX";
+                }
+            }
+            for (int i = 0; i < R.Count; i++)
+            {
+                for (int j = 0; j < message.Length; j++)
+                {
+                    if (message[j] != "XXX")
+                    {
+                        char[] arr = R[i].ToCharArray();
+                        Array.Reverse(arr);
+                        string iR = new string(arr);
+                        var am = message[j].Split(':');
+                       //Console.WriteLine("{0} <> {1}", iR, am[1]);
+                        if (iR == am[1])
+                        {
+                            message[j] = "XXX";
+                        }
+                    }
+                }
+            }
+            string outp = "";
+            foreach (var item in message)
+            {
+                if (item != "XXX")
+                {
+                    outp =  item;
+                }
+            }
+            return outp.Substring(2);
+        }
+
+        static void Main(string[] args)
+        {
+            string[] str = {"S:zsncf5314198IZOFI","R:IFOZI8914135fcnsz","S:zszjo5857391DWTUC","R:CUTWD1937585ojzsz","S:xiidj2356366MMFHW","R:WHFMM6636532jdiix","S:ekprc3898874TNIXT","R:TXINT4788983crpke","S:lmekh3048939FUQRI","R:IRQUF9398403hkeml","S:bfamh3815909XYJSN","R:NSJYX9095183hmafb","S:lrlwf732281DVSFE","R:EFSVD182237fwlrl","S:djbdd9505876JJXGL","R:LGXJJ6785059ddbjd","S:eztkx5976243EYEBM","R:MBEYE3426795xktze","S:oiknf3721519CPEUG","R:GUEPC9151273fnkio","S:vfomj3540390OTSOR","R:ROSTO0930453jmofv","S:utamm2915264HRMBG","R:GBMRH4625192mmatu","S:mlxmh6546043HHAEU","R:UEAHH3406456hmxlm","S:swiya8475751NXIOD","R:DOIXN1575748ayiws","S:apzbl5041188WDEAZ","R:ZAEDW8811405lbzpa","S:xqnih9555848TJOAW","R:WAOJT8485559hinqx","S:rkoac5421667JTACC","R:CCATJ7661245caokr","S:aujhi421538WDYHM","R:MHYDW835124ihjua","S:ikmbo5898298HUPFM","R:MFPUH8928985obmki","S:skkod5177433SXVXZ","R:ZXVXS3347715dokks","S:HaveFunWithCodeLearn"};
+            string[] str2 = { "S:ab", "S:bc", "R:ba" };
+            Console.WriteLine(LostMessage(str));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dot NET/CodeLearn/RemovedDigit/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemovedDigit
+{
+    class Program
+    {
+        static int RemovedDigit(int n, int[] s, int k)
+        {
+            String inp = "";
+            for (int i = 1; i <= n; i++)
+            {
+                inp += i + "";
+            }
+            int outp = -1;
+            int run = 0;
+            int temp = k;
+            while (k != 0)
+            {
+                if (run >= inp.Length-1) return -1;
+                for (int i = run; i < inp.Length; i++)
+                {
+                    bool check = false;
+                    for (int j = 0; j < s.Length; j++)
+                    {
+                        if (int.Parse(inp[i]+"") == s[j])
+                        {
+                            outp = int.Parse(inp[i] + "");
+                            check = true;
+                            break;
+                        }
+                    }
+                    if(check)
+                    {
+                        run = i + 1;
+                        k--;
+                        break;
+                    }
+                    if (i == inp.Length-1 && k >= 1)
+                    {
+                        return -1;
+                    }
+                }
+            }
+            return outp;
+        }
+
+        static void Main(string[] args)
+        {
+            //12 34 56 78 9101112 1314 15
+            //
+            int[] x = {2,3};
+            Console.WriteLine(RemovedDigit(4, x, 3));
+            Console.ReadKey();
+        }
+    }
+}
